3eb585347e41ba2a16b5f8da748f2a66b9b4125c
almokhtar <>
2020-05-06 00:40:21 +0000

    wip jwt

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+Route::post('register', 'AuthController@register');
+Route::post('login', 'AuthController@login');
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => 'jwt.verify'], function () {
+    Route::post('logout', 'AuthController@logout');
+    Route::post('me', 'AuthController@me');
 });
