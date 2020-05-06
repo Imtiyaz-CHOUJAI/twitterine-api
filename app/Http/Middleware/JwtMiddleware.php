1a4ae931b1c025e086109c916ab960338e3bc708
almokhtar <>
2020-05-06 00:47:05 +0000

    adding a middleware

---
+++ app/Http/Middleware/JwtMiddleware.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class JwtMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        // this middleware doing nothing for now TODO later
+        return $next($request);
+    }
+}
